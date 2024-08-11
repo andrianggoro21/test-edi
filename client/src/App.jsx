36b72eecf0e66168778b92ed5e370906bebc2daf
Andri Anggoro <andri.anggoro21@gmail.com>
2024-08-12 00:08:43 +0700

    fix: FE update router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
@@ -6,11 +6,31 @@ import CreateBiodata from "./pages/biodata/CreateBiodata";
 import EditBiodata from "./pages/biodata/EditBiodata";
 import Navbar from "./components/Navbar";
 import Auth from "./components/auth/Auth";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 function App() {
   const location = useLocation();
+  const navigate = useNavigate();
+  const { user } = useSelector((state) => state.AuthReducer);
 
   const showNavbar = !["/login", "/register"].includes(location.pathname);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      if (["/login", "/register"].includes(location.pathname)) {
+        if (user && user.id) {
+          navigate(`/dashboard/${user.id}`);
+        }
+      }
+    } else {
+      if (["/login", "/register"].includes(location.pathname)) {
+        // Tidak perlu navigasi
+      } else {
+        navigate("/login");
+      }
+    }
+  }, [navigate, user, location.pathname]);
 
   return (
     <>
